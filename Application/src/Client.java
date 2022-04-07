431eba7cd6d5e4b1e236d6639a312406ca73383e
Chermas <victor.r.andersen99@gmail.com>
2022-04-07 16:28:45 +0200

    Client event and enabling handled

--- Application/src/Client.java
+++ Application/src/Client.java
@@ -6,35 +6,47 @@ import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.EthFilter;
 
 import io.reactivex.Flowable;
+import net.sf.saxon.expr.instruct.Choose;
 import ethereum.*;
-import ethereum.Storage.ChangeStrategyEventResponse;
+import ethereum.SixG_Strategy.AddStrategyEventResponse;
+import ethereum.SixG_Strategy.Strategy;
 
 public class Client {
-    static Storage storage;
-    static Strategy strategy;
+    static SixG_Strategy storage;
+    static Strategy active_strategy;
+    static List<Strategy> strategies;
+    Runtime runtime = Runtime.getRuntime();
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         StorageCreater sc = new StorageCreater();
         storage = sc.create();
-        Flowable<ChangeStrategyEventResponse> flow = storage.changeStrategyEventFlowable(DefaultBlockParameterName.LATEST, DefaultBlockParameterName.LATEST);
+        setStartegies();
+        Flowable<AddStrategyEventResponse> flow = storage.addStrategyEventFlowable(DefaultBlockParameterName.LATEST, DefaultBlockParameterName.LATEST);
         flow.subscribe(event -> {
-            List<Strategy> strategies = event.strategies;
-            
+            SixG_Strategy.Strategy strategy = event.strategy;
+            strategies.add(strategy);
+            active_strategy = chooseActiveStrategy();
+            enableStrategy();
         });
     }
 
-    public void updateStrategy(int startDay, int startMonth, int startYear, int endDay, int endMonth, int endYear, int priority, int x, int y, int radius, int connectionType){
-        strategy.setStartDate(startDay, startMonth, startYear);
-        strategy.setEndDate(endDay, endMonth, endYear);
-        strategy.setPriority(priority);
-        strategy.setConnectionType(connectionType);
-        strategy.setLocation(x, y, radius);
+    public static void setStartegies() throws Exception{
+        strategies = storage.getStrategies().send();
     }
 
-    public Strategy chooseActiveStrategy(){
-        Strategy strat = null;
-        return strat;
+    public static Strategy chooseActiveStrategy(){
+        long now = System.currentTimeMillis() / 1000L;
+        for(Strategy strat : strategies){
+            if(now > strat.startDate.longValue() && now < strat.endDate.longValue() && strat.priority.intValue() > active_strategy.priority.intValue()) {
+                return strat;
+            }
+        }
+        return null; 
+    }
+
+    public static void enableStrategy(){
+        System.out.println("Connection: " + active_strategy.connectionType);
     }
 
 }
